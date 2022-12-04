53e6058c8d7a0ffeaa2da3f7b69aaf7966214f2c
buldubu <buldubu@gmail.com>
2022-12-04 22:24:30 +0300

    Adding test id to login components

--- web/components/Login/index.js
+++ web/components/Login/index.js
@@ -58,6 +58,7 @@ export default function Login() {
                   id="email"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
+                  data-testid="email"
                   required
                 />
               </div>
@@ -77,6 +78,7 @@ export default function Login() {
                   id="password"
                   placeholder="••••••••"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  data-testid="password"
                   required
                 />
               </div>
