8ccf45497d83eadc72a810dc3afe768107d7b61f
Mücahit Eren Cantemür <eren.cantemur@jekirdek.com>
2022-12-04 17:40:05 +0300

    Login and Base post request

--- web/components/Login/index.js
+++ web/components/Login/index.js
@@ -1,6 +1,25 @@
 import Image from "next/image";
+import { useState } from "react";
+import loginRequest from "../../requests/login";
+import Cookie from "js-cookie";
+import { COOKIENAME } from "../../config/cookie.config";
 
 export default function Login() {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const handleSubmit = async () => {
+    event.preventDefault();
+    
+    const { message, result } = await loginRequest(email, password);
+    
+    if (result?.token) {
+      Cookie.set(COOKIENAME, token);
+    }
+    
+    console.log(message);
+  };
+
   return (
     <section class="bg-gray-50 dark:bg-gray-900">
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -31,12 +50,15 @@ export default function Login() {
                   Your email
                 </label>
                 <input
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                  }}
                   type="email"
                   name="email"
                   id="email"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
-                  required=""
+                  required
                 />
               </div>
               <div>
@@ -47,12 +69,15 @@ export default function Login() {
                   Password
                 </label>
                 <input
+                  onChange={(event) => {
+                    setPassword(event.target.value);
+                  }}
                   type="password"
                   name="password"
                   id="password"
                   placeholder="••••••••"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  required
                 />
               </div>
               <div class="flex items-center justify-between">
@@ -83,7 +108,7 @@ export default function Login() {
                 </a>
               </div>
               <button
-                type="submit"
+                onClick={handleSubmit}
                 class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Sign in
