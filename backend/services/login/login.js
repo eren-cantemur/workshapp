06dbea1d1690596f48b70a8c4bb2c0afd186b1a8
Mücahit Eren Cantemür <eren.cantemur@jekirdek.com>
2022-12-04 15:42:59 +0300

    Built login and register services

--- backend/services/login/login.js
+++ backend/services/login/login.js
@@ -1,17 +1,68 @@
 import { User } from "../../models/user";
-import { jwt } from "jsonwebtoken";
+import { Customer } from "../../models/customer";
+import { WorkshopManager } from "../../models/workshopManager";
+import { Admin } from "../../models/admin";
+
+import * as bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import { JWTPRIVATEKEY } from "../../config/jwt.config";
 
 login = async (email, password) => {
   const user = await User.findOne({ email: email });
 
   if (user === null) {
     return {
+      type: "Error",
+      message: "Couldn't find user registered with this email.",
+    };
+  } else {
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user.toJSON().password
+    );
+
+    if (!isPasswordCorrect) {
+      return {
         type: "Error",
-        message: "Couldn't find user registered with this email."
+        message: "Invalid password.",
+      };
+    } else {
+      var role = "";
+      
+      const admin = Admin.findOne({ userID: user.id });
+
+      if (admin) {
+        role = "admin";
+      } else {
+        const workshopManager = WorkshopManager.findOne({ userId: user.id });
+
+        if (workshopManager) {
+          role = "workshopManager";
+          
+        } else {
+          const customer = Customer.findOne({ userId: user.id });
+
+          if (customer) {
+            role = "customer";
+          }
+        }
+      }
+
+      const privateKey = JWTPRIVATEKEY;
+
+      const token = await jwt.sign(
+        { userID: user.id, role: role },
+        privateKey,
+        { algorithm: "RS256" },
+        { expiresIn: "14d" }
+      );
+
+      return {
+        type: "Success",
+        message: "Successfully logged you in.",
+        result: token,
+      };
     }
-  }
-  else {
-    
   }
 };
 
--- backend/services/register/register.js
+++ backend/services/register/register.js
@@ -1,8 +1,16 @@
+import { SALTROUNDS } from "../../config/bcrypt.config";
+import { Admin } from "../../models/admin";
+import { Customer } from "../../models/customer";
 import { User } from "../../models/user";
-import { jwt } from "jsonwebtoken";
+import { WorkshopManager } from "../../models/workshopManager";
 
-register = async (email, password) => {
+import * as bcrypt from "bcrypt";
+
+register = async (email, password, role) => {
   const user = await User.findOne({ email: email });
+  const hashedPassword = await bcrypt.hash(password, SALTROUNDS);
+
+  var newUser;
 
   if (user !== null) {
     return {
@@ -10,11 +18,45 @@ register = async (email, password) => {
       message: "An user is already registered with this email.",
     };
   } else {
-    const user = User.create({ email, password });
+    if (role === "customer") {
+      newUser = Customer.create({
+        name: "",
+        photo: "",
+        user: {
+          email: email,
+          password: hashedPassword,
+        },
+      });
+    } else if (role === "admin") {
+      newUser = Admin.create({
+        name: "",
+        user: {
+          email: email,
+          password: hashedPassword,
+        },
+      });
+    } else if (role === "workshopManager") {
+      newUser = WorkshopManager.create({
+        name: "",
+        logo: "",
+        phone: "",
+        user: {
+          email: email,
+          password: hashedPassword,
+        },
+      });
+    } else {
+      return {
+        type: "Error",
+        message: "Couldn't identify role.",
+      };
+    }
 
     return {
       type: "Success",
-      message: `User is registered with email: ${user.toJSON().email}`,
+      message: `User is registered as ${role} with email: ${
+        newUser.toJSON().user.email
+      }`,
     };
   }
 };
