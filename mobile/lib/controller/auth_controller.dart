5d4ec267b7dec139ab5ac9f7b57ce02be3b4e0f1
emre <kilince18@itu.edu.tr>
2022-12-05 19:03:29 +0300

    Revert "Merge pull request #4 from eren-cantemur/emre"
    This reverts commit a2535cf13d1b80a1edf7e23dcde436867fb58ded, reversing
    changes made to b57c48a3763e189da94c0d8f8fc934715bc40ef7.

--- mobile/lib/controller/auth_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class AuthController extends ChangeNotifier {
-  static void navigateToNextPage(String nextPageId, BuildContext context) {
-    Navigator.pushNamed(context, nextPageId);
-  }
-}
--- mobile/lib/model/auth_user_model.dart
+++
@@ -1,6 +0,0 @@
-class AuthUser {
-  AuthUser(this.email, this.password, this.jwt);
-  final String email;
-  final String password;
-  final String jwt;
-}
