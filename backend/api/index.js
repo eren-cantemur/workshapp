99ff55d0ffda94156c0904b4b5469330b723ff72
Alperen Aydın <aydina19@itu.edu.tr>
2022-12-16 10:05:59 +0300

    add time&reservation routes

--- backend/api/index.js
+++ backend/api/index.js
@@ -8,6 +8,8 @@ const adminRouter = require("./admin")
 const customerRouter = require("./customer")
 const workShopManagerRouter = require("./workShopManager")
 const addressRouter = require("./address")
+const timeRouter = require("./time")
+const reservationRouter = require("./reservation")
 
 router.use("/user", userRouter)
 router.use("/admin", adminRouter)
@@ -16,5 +18,7 @@ router.use("/workShopManager", workShopManagerRouter)
 router.use("/workshop", workShopRouter)
 router.use("/review", reviewRouter)
 router.use("/address", addressRouter)
+router.use("/time", timeRouter)
+router.use("/reservation", reservationRouter)
 
 module.exports = router
