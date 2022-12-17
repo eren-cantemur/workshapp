da67918a56c9760bee71c3295c9355d27cbeaf2e
Alperen Aydın <aydina19@itu.edu.tr>
2022-12-17 13:43:13 +0300

    bugfix

--- backend/models/reservation.js
+++ backend/models/reservation.js
@@ -4,8 +4,8 @@ module.exports = (sequelize, Sequelize) => {
     UserModel = User(sequelize,Sequelize)
     WorkshopModel = Workshop(sequelize,Sequelize)
     const Reservation = sequelize.define("reservation", {
-        repetation : {
-            date : Sequelize.DATE
+        date : {
+            type : Sequelize.DATE
         }
     })
     Reservation.belongsTo(UserModel)
