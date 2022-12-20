a50435bbb3e7f70c3340a95918d76c05bf88a39b
Alperen Aydın <aydina19@itu.edu.tr>
2022-12-20 14:46:10 +0300

    bugfix

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,11 +2,13 @@ const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
-  dialect: dbConfig.DIALECT,
+  dialect: 'mysql',
   port: 3306,
   dialectOptions: {
     ssl: 'Amazon RDS'
-  }
+  },
+  
+  
 });
 
 const db = {};
@@ -22,6 +24,7 @@ db.Admin = require("./admin.js")(sequelize, Sequelize);
 db.Customer = require("./customer.js")(sequelize, Sequelize);
 db.WorkshopImage = require("./workshopImage.js")(sequelize, Sequelize);
 db.Reservation = require("./reservation.js")(sequelize, Sequelize);
+db.Time = require("./time.js")(sequelize, Sequelize);
 
 
 
