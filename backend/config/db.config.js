0f5795b3c8f710f81155ac02ab14d228a585f4fb
Mücahit Eren Cantemür <eren.cantemur@jekirdek.com>
2022-12-21 21:15:15 +0300

    user -> username

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     DB : process.env.DB,
     HOST : process.env.HOST,
-    USER : process.env.USER,
+    USERNAME : process.env.USERNAME,
     PASSWORD : process.env.PASSWORD,
     DBPORT : process.env.DBPORT,
     PORT : process.env.PORT,
