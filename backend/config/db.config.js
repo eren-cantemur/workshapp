51e1e4ecfa3755c9fd1eb0ac3bcd8574343cc7e7
Mücahit Eren Cantemür <eren.cantemur@jekirdek.com>
2022-12-21 21:16:47 +0300

    some env changes

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,9 +1,9 @@
 
 module.exports = {
     DB : process.env.DB,
-    HOST : process.env.HOST,
-    USERNAME : process.env.USERNAME,
-    PASSWORD : process.env.PASSWORD,
+    DBHOST : process.env.DBHOST,
+    DBUSERNAME : process.env.DBUSERNAME,
+    DBPASSWORD : process.env.DBPASSWORD,
     DBPORT : process.env.DBPORT,
     PORT : process.env.PORT,
     DIALECT : process.env.DIALECT
--- backend/models/index.js
+++ backend/models/index.js
@@ -1,8 +1,8 @@
 const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USERNAME, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.DBUSERNAME, dbConfig.DBPASSWORD, {
+  host: dbConfig.DBHOST,
   port: dbConfig.DBPORT,
   dialect: "mysql",
   ssl: "Amazon RDS",
