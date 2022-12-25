0db72ea3828d86d7ffd2b6d054a56abae235a444
emre <kilince18@itu.edu.tr>
2022-12-25 16:27:27 +0300

    fix context possible crash

--- mobile/lib/controller/feed_data_provider.dart
+++ mobile/lib/controller/feed_data_provider.dart
@@ -12,32 +12,32 @@ class FeedDataProvider with ChangeNotifier {
   List<Workshop> _data = [];
   List<Workshop> get data => _data;
 
-  FeedDataProvider(context) {
+  FeedDataProvider(BuildContext context) {
     _fetchData(context);
   }
 
-  Future<void> _fetchData(context) async {
+  Future<void> _fetchData(BuildContext context) async {
     updateData(context);
   }
 
-  void updateData(context) async {
+  void updateData(BuildContext context) async {
     _data = await getWorkshops(context);
     notifyListeners();
   }
 
-  Future<List<Workshop>> getWorkshops(context) async {
+  Future<List<Workshop>> getWorkshops(BuildContext context) {
     if (Provider.of<JWTProvider>(context, listen: false).jwt != null) {
-      final workshops = await NetworkController.getWorkshops(Provider.of<JWTProvider>(context, listen: false).jwt!);
-      return workshops;
+      return NetworkController.getWorkshops(Provider.of<JWTProvider>(context, listen: false).jwt!)
+          .then((value) => value);
     } else {
-      String? readedJWT = await LocalDataController.readJWT();
-      if (readedJWT == null) {
-        await AuthController.logout(context);
-        return [];
-      }
-      Provider.of<JWTProvider>(context, listen: false).getToken();
-      final workshops = await NetworkController.getWorkshops(readedJWT);
-      return workshops;
+      return LocalDataController.readJWT().then((key) {
+        if (key == null) {
+          AuthController.logout(context);
+          return [];
+        }
+        Provider.of<JWTProvider>(context, listen: false).getToken();
+        return NetworkController.getWorkshops(key);
+      });
     }
   }
 }
