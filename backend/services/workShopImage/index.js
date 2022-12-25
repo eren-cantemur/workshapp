5e16278be04bec8b9713971d64da412ece29d500
AlperenAydın <aydina19@itu.edu.tr>
2022-12-25 17:11:29 +0300

    Delete backend/services/workShopImage directory

--- backend/services/workShopImage/index.js
+++
@@ -1,5 +0,0 @@
-exports.create = require('./create').create
-exports.getAll = require('./getAll').getAll
-exports.getByWorkshopId = require('./getByWorkshopId').getByWorkshopId
-exports.update = require('./update').update
-exports.delete = require('./delete').delete
