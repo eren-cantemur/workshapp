b9cac48e0e18de2a2a312f37ff79cefb599e59a9
AlperenAydın <aydina19@itu.edu.tr>
2022-12-25 17:11:37 +0300

    Delete backend/services/workShopManager directory

--- backend/services/workShopManager/index.js
+++
@@ -1,5 +0,0 @@
-exports.getAll = require('./getAll').getAll
-exports.getById = require('./getById').getById
-exports.getByName = require('./getByName').getByName
-exports.update = require('./update').update
-exports.delete = require('./delete').delete
