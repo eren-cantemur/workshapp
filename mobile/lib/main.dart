0e06075939e00660b811d969cd70ab999a303210
emre <kilince18@itu.edu.tr>
2023-01-10 22:51:28 +0300

    fix old missing files

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -43,11 +43,6 @@ class MyApp extends StatelessWidget {
         },
         home: const WelcomePage(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
